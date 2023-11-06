3833aed3e3f2b4115444ae6065e64f7caea383b8
Villa-VillaDiana <diana.villavilla02@gmail.com>
2023-11-06 12:40:38 +0800

    Fix indentation and spaces

--- villa-villa_diana/script.js
+++ villa-villa_diana/script.js
@@ -1,7 +1,6 @@
 function newComment() {
   let nameField = document.getElementById("name");
   let commentField = document.getElementById("user_comment");
-
   if (nameField.value.length > 0) {
     if (commentField.value.length > 0) {
       document.getElementById("comment_btn").disabled = false;
